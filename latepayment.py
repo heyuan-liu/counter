60e84a89a85a092c0664bf7fb9325b2d162911c4
Snail <86354684+newyuser@users.noreply.github.com>
2022-10-07 23:59:16 +0800

    Add files via upload

---
+++ latepayment.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+
+## 无投资计划
+
+# ---------------------------------------------------
+# 输入贷款总额pv、贷款利率rate、还款期限period
+# 输出每月还款额payment、缴纳利息总额interest
+# ---------------------------------------------------
+def get_payment(pv, rate, period):
+    # 贷款月利率r
+    r = ((1 + rate/12)**12 - 1)/12
+    if r == 0:
+        return pv / period, 0
+    
+    payment = (pv * r) / (1 - (1 / (1 + r))**period)
+    interest = payment * period - pv
+    
+    return payment, interest
+
+# ---------------------------------------------------
+# 输入贷款总额pv、贷款利率rate、每月还款额payment
+# 输出还款期限period、缴纳利息总额interest
+# ---------------------------------------------------
+def get_period(pv, rate, payment):
+    # 贷款月利率r
+    r = ((1 + rate/12)**12 - 1)/12
+    if r == 0:
+        return np.ceil(pv / payment), 0
+    
+    period = - np.log(1 - pv * r / payment) / np.log(1 + r)
+    interest = payment * period - pv
+    
+    return np.ceil(period), interest
+
+
+## 有投资计划
+
+# ---------------------------------------------------
+# 输入贷款总额pv、贷款利率rate、购房基金初始资金init_fund、投资收益率ret、还款期限period
+# 输出每月基金支出payment_fund_actual、每月额外支出payment_add、缴纳利息总额interest
+# ---------------------------------------------------
+def get_payment_w_invest(pv, rate, init_fund, ret, period):
+    # 贷款月利率r
+    r = ((1 + rate/12)**12 - 1)/12
+    if r == 0:
+        return pv / period, 0
+    
+    payment = (pv * r) / (1 - (1 / (1 + r))**period) # total payment
+    interest = payment * period - pv
+    
+    # 投资月收益率g
+    g = (1 + ret)**(20/260) - 1
+    if g == 0:
+        return payment, interest
+    
+    # 从购房基金中每月最大支出金额payment_fund
+    payment_fund = init_fund * g / (1 - (1 / (1 + g))**period)
+    payment_fund_actual = min(payment, payment_fund)
+    payment_add = max(0, payment - payment_fund)
+    
+    return payment_fund_actual, payment_add, interest
+
+# ---------------------------------------------------
+# 输入贷款总额pv、贷款利率rate、购房基金初始资金init_fund、投资收益率ret、每月还款额payment
+# 输出还款期限period、每月基金支出payment_fund_actual，每月额外支出payment_add、缴纳利息总额interest
+# 注：必须输入还款额总额，因为只输入payment_fund_actual或payment_add无法确定还款期限
+# ---------------------------------------------------
+def get_period_w_invest(pv, rate, init_fund, ret, payment):
+    # 贷款月利率r
+    r = ((1 + rate/12)**12 - 1)/12
+    if r == 0:
+        return pv / period, 0
+    
+    period = - np.log(1 - pv * r / payment) / np.log(1 + r)
+    interest = payment * period - pv
+    
+    # 投资月收益率g
+    g = (1 + ret)**(20/260) - 1
+    if g == 0:
+        return period, interest
+    
+    # 从购房基金中每月最大支出金额payment_fund
+    payment_fund = init_fund * g / (1 - (1 / (1 + g))**period)
+    payment_fund_actual = min(payment, payment_fund)
+    payment_add = max(0, payment - payment_fund)
+    
+    return payment_fund_actual, payment_add, interest
+
+# ---------------------------------------------------
+# 也可以先用无投资计划的方法计算出每月还款额payment、还款期限period
+# 再直接根据购房基金初始资金init_fund、投资收益率ret、每月还款额payment、还款期限period
+# 从而计算每月基金支出payment_fund_actual，每月额外支出payment_add
+# ---------------------------------------------------
+def separate_payment(init_fund, ret, payment, period):
+    g = (1 + ret)**(20/260) - 1
+    if g == 0:
+        return period, interest
+    
+    # 从购房基金中每月最大支出金额payment_fund
+    payment_fund = init_fund * g / (1 - (1 / (1 + g))**period)
+    payment_fund_actual = min(payment, payment_fund)
+    payment_add = max(0, payment - payment_fund)
+    
+    return payment_fund_actual, payment_add
+
--- pro.py
+++ pro.py
@@ -1,57 +1,75 @@
-from matplotlib.cbook import ls_mapper
 import streamlit as st
 import pandas as pd
+from latepayment import *
+import numpy
 
 #左侧输入框
 st.sidebar.subheader('输入贷款金额')
 total_price = st.sidebar.number_input(
     "房/车总价(万元)",
-    value=100
+    value = 100
 )
 down_payment = st.sidebar.number_input(
     "首付(万元)",
-    value=40
+    value = 40
 )
 
 st.sidebar.subheader('输入贷款细节')
-loan_rate = st.sidebar.number_input(
+st.sidebar.info('还款期限/每月还款额 二选一输入')
+rate = st.sidebar.number_input(
     "贷款利率(%)",
-    help='???',
-    value=3
+    help = '???',
+    value = 3
 )
-term_repayment = st.sidebar.number_input(
+period = st.sidebar.number_input(
     "还款期限(月)",
-    value=48
+    value = 48
 )
-monthly_repayment = st.sidebar.number_input(
-    "每月还款(元)",
-    value=6000
+payment = st.sidebar.number_input(
+    "每月还款额(元)",
+    value = 0
 )
 
 st.sidebar.subheader('输入投资计划')
-investment_initial_capital = st.sidebar.number_input(
+init_fund = st.sidebar.number_input(
     "投资初始资金(万元)",
-    help="???",
-    value=12
+    help = "???",
+    value = 0
 )
-ROL = st.sidebar.number_input(
+ret = st.sidebar.number_input(
     "投资收益率(%)",
-    help='年化',
-    value=8
+    help ='年化',
+    value = 0
 )
+
+
+
+#计算
+pv = (total_price - down_payment) * 10000
+rate /= 100
+if init_fund == 0 and ret == 0:
+    if payment == 0 and period != 0:
+        payment, interest = get_payment(pv, rate, period)
+    if period == 0 and payment != 0:
+        period, interest = get_period(pv, rate, payment)
+    payment_fund_actual, payment_add = 0, 0
+if init_fund != 0 and ret != 0:
+    init_fund *= 10000
+    ret /= 100
+    if payment == 0 and period != 0:
+        payment_fund_actual, payment_add, interest = get_payment_w_invest(pv, rate, init_fund, ret, period)
+    if period == 0 and payment != 0:
+        payment_fund_actual, payment_add, interest = get_period_w_invest(pv, rate, init_fund, ret, payment)
 
 
 #右侧方案栏
-total_loan = total_price - down_payment
-total_interest = 0
-monthly_initial_capital = 0
-indexlist = ['房/车总价','首付','贷款总额','贷款利率','还款期限','每月还款','缴纳利息总额','投资初始资金','投资收益率（年化）','每月投入资金']
-datalist = [total_price,down_payment,total_loan,loan_rate,term_repayment,monthly_repayment,total_interest,investment_initial_capital,ROL,monthly_initial_capital]
+indexlist = ['房/车总价','首付','贷款总额','贷款利率','还款期限','每月还款额','缴纳利息总额','投资初始资金','投资收益率（年化）','每月投入资金','每月额外支出']
+datalist = [total_price*10000,down_payment*10000,pv,rate,period,payment,interest,init_fund,ret,payment_fund_actual,payment_add]
 cur_pd = pd.DataFrame(
     {
         '详细数据': datalist,
     }
-    , index=[i for i in indexlist]
+    , index = indexlist
 )
 st.title('贷款计算器')
 st.header('方案详情')
@@ -61,11 +79,32 @@ programe_name = st.text_input(
 )
 st.table(cur_pd)
 
-temp_pd = cur_pd
 if st.button('保存方案'):
-    last_pd = cur_pd
-else:
-    last_pd = temp_pd
+    f = open("./data.txt","w")
+    f.write(str(programe_name) + '\n')
+    for line in datalist:
+        f.write(str(line) + '\n')
+    f.close()
+
+fp = open('./data.txt','r')
+last_data=[]
+flag = 0
+for line in fp:
+    line=line.strip('\n') 
+    if flag == 0:
+        programe_name = line
+    else:
+        last_data.append(line)
+    flag += 1   
+
+fp.close()
+last_data = numpy.array(last_data,dtype=float)   
+last_pd = pd.DataFrame(
+    {
+        '详细数据': last_data,
+    }
+    , index = indexlist
+)
 st.header('上个方案')
 last_programe_name = st.text_input(
     "方案名称",
